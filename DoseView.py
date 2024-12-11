bc2fd1836173799b20b8c8056bdc21d97284f84b
Gwen <123043612+gwenethgray@users.noreply.github.com>
2024-12-10 22:33:45 -0500

    Update DoseView.py

--- DoseView.py
+++ DoseView.py
@@ -15,7 +15,7 @@ from pydicom import dcmread
 from matplotlib.figure import Figure
 import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from tkinter import filedialog, W
+from tkinter import filedialog
 from tkinter.ttk import *
 
 root = tk.Tk()
@@ -25,7 +25,7 @@ root.minsize(750,450)
 
 filename = tk.StringVar(root)
 e1 = Entry(root, textvariable=filename)
-e1.grid(row=0, column=1, columnspan=2)
+e1.grid(row=0, column=1, columnspan=4, sticky="ew")
 e1.config(state="readonly")
 
 def browse():
@@ -33,18 +33,19 @@ def browse():
     filename.set(f)
 
 b1 = Button(root, text="Select Dose File", command=browse)
-b1.grid(row=0, column=0)
+b1.grid(row=0, column=0, columnspan=1, sticky="ew")
 
 fig = Figure(figsize=(8,6), dpi=100)
 ax = fig.add_subplot(111)
 canvas = FigureCanvasTkAgg(fig, master=root)
-canvas.get_tk_widget().grid(row=1, column=1, columnspan=2)
+canvas.get_tk_widget().grid(row=1, column=0, columnspan=6)
 canvas.draw()
 
 slices, rows, cols = [0, 0, 0]
 ind = slices//2
 data = None
 im = None
+cbar = None
     
 def onscroll(event):
     global ind, slices, ax, im
@@ -59,15 +60,21 @@ def onscroll(event):
 fig.canvas.mpl_connect('scroll_event', onscroll)
     
 def plot_dcm():
-    global slices, rows, cols, ind, im, ax, data
+    global slices, rows, cols, ind, im, ax, data, cbar
+    if cbar is not None:
+        cbar.remove()
+    ax.clear()
     ds = dcmread(e1.get())
-    data = ds.pixel_array
+    data = ds.pixel_array * ds.DoseGridScaling
     slices, rows, cols = data.shape
     ind = slices//2
     im = ax.imshow(data[ind, :, :])
+    cbar = fig.colorbar(im, ax=ax, location="bottom")
+    cbar.set_label("Dose (Gy)")
+    ax.set_title(f"Plan: \"{ds.SeriesDescription}\", Patient: \"{ds.PatientID}\"")
     canvas.draw()
     
 b2 = Button(root, text="Plot", command=plot_dcm)
-b2.grid(row=1, column=0)
+b2.grid(row=0, column=5, columnspan=1, sticky="ew")
 
 root.mainloop()
